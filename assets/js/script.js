b2d9c0b9fda887addeaf729e4e2c9e7e3578c79a
glmfrk <golamfaruk204@gmail.com>
2026-09-06 14:15:06 +0600

    mobile menu implement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,20 @@
-const newTaskInput = document.getElementById('new_task');
-const addTaskButton = document.getElementById('add_task');
-const todoList = document.querySelector('.todo_list .items');
-const completeList = document.querySelector('.complete_list .items');
+const hamburgerBtn = document.getElementById('kk_hamburger_btn');
+const closeBtn = document.getElementById('kk_drawer_close_btn');
+const mobileDrawer = document.getElementById('kk_mobile_drawer');
+const mobileOverlay = document.getElementById('kk_mobile_overlay');
 
-
-
-function addTask(event) {
-   event.preventDefault();
-
-   const cleanedTask = newTaskInput.value.trim();
-
-   if (!cleanedTask) {
-      console.log(cleanedTask);
-   
-      newTaskInput.focus();
-		return;
-   }
-   createTaskItem(cleanedTask);
+function openDrawer() {
+  mobileDrawer.classList.add('is-active');
+  mobileOverlay.classList.add('is-active');
+  document.body.style.overflow = 'hidden'; // স্ক্রোল বন্ধ করতে
 }
 
-
-addTaskButton.addEventListener('click', addTask);
-
-function createTaskItem(cleanedTask) {
-   const item = document.createElement('li');
-   const checkbox = document.createElement('input');
-   const label = document.createElement('label');
-   const actionButton = document.createElement('button');
-
-   checkbox.type = 'checkbox';
+function closeDrawer() {
+  mobileDrawer.classList.remove('is-active');
+  mobileOverlay.classList.remove('is-active');
+  document.body.style.overflow = '';
 }
+
+hamburgerBtn?.addEventListener('click', openDrawer);
+closeBtn?.addEventListener('click', closeDrawer);
+mobileOverlay?.addEventListener('click', closeDrawer);
